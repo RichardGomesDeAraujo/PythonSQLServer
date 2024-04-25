bef1c463a45f5e11cbd8b3bc76bcb99099e0c4c7
Richard Gomes de Araujo <84196206+RichardGomesDeAraujo@users.noreply.github.com>
2024-04-25 20:41:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import pyodbc
+
+## Criar a conexão com os dados do banco de dados 
+## Importante manter o sinal de = sem espaços
+dadosconexao = (
+    "Driver={SQL Server};"
+    "Server=BOOK-J33GD56FP6;"
+    "Database=DBBV;"
+)
+
+## Efetivar a conexão com o comando pyodbc
+conexao = pyodbc.connect(dadosconexao)
+
+## Imprimir uma mensagem caso a conexão seja bem sucedida
+print("Conexão Bem Sucedida")
+
+## Criar uma "Nova Consulta" para rodar comandos no SQL Server
+cursor = conexao.cursor()
+
+## Criando variáveis para inserir dados dinâmicos na tabela
+NrPasta = 3002
+NrRequisicao = 3002
+DtRequisicao = "12/08/2023"
+DtExecucao = "13/08/2023"
+StRequisicao = "Concluída"
+Login = "S"
+PzSla = 2
+PzExecucao = 1
+FlConformidade = "S"
+NmAssessoria = "Gary"
+
+## Criar os comandos para rodar no SQL
+comando = f"""
+INSERT INTO TbJuridicoEncerramento
+VALUES
+(
+{NrPasta},{NrRequisicao},'{DtRequisicao}','{DtExecucao}','{StRequisicao}','{Login}',{PzSla},{PzExecucao},'{FlConformidade}','{NmAssessoria}'
+)
+"""
+
+## Criar Executável do comando
+cursor.execute(comando)
+
+## Se for executado algum comando de criação ou alteração do banco de dados
+## será necessário o comando de commit abaixo:
+cursor.commit()
+
+
